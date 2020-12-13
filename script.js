5a4ca76e6ec6040d802e6882db4314be1d0d13cb
VinceH28 <VincentHart28@gmail.com>
2020-12-13 04:58:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const calender = document.createElement("calender");
-const month = document.createElement("month");
 const Sunday = document.createElement("Weekdays");
-const nine = document.createElement("01");
-const ten = document.createElement("02");
-const eleven = document.createElement("03");
-const twelve = document.createElement("04");
-const one = document.createElement("05");
-const two = document.createElement("06");
-const three = document.createElement("07");
-const four = document.createElement("08");
-const five = document.createElement("09");
+// const nine = document.createElement("01");
+// const ten = document.createElement("02");
+// const eleven = document.createElement("03");
+// const twelve = document.createElement("04");
+// const one = document.createElement("05");
+// const two = document.createElement("06");
+// const three = document.createElement("07");
+// const four = document.createElement("08");
+// const five = document.createElement("09");
+
+const date = new Date();
+
+
+const month = date.getMonth();
+
+console.log(month);
+// [
+//     "January",
+//     "February",
+//     "March",
+//     "April",
+//     "May",
+//     "June",
+//     "July",
+//     "August",
+//     "September",
+//     "October",
+//     "November",
+//     "December",
+// ];
+
+// document.querySelector('monthName.h1').innerHTML = months [12];
+
+// const saveBtn = $(".saveBtn");
