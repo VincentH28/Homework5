7e1c6cc087f30ff9be13e413f5956ecbd81948a2
VincentH28 <73213916+VinceH28@users.noreply.github.com>
2021-04-22 09:37:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+//Date 
+$("#currentDay").text(luxon.DateTime.local().toLocaleString({
+    weekday: "long",
+    month: "long",
+    day: "2-digit",
+  })
+  );   
+  
+function time(){
+let fixedHour = parseInt(moment().format("H"));
+  
+//Determine timeblock color based on am or pm
+$(".time-block").each(function(){
+let presentHour = parseInt($(this).attr("id"));
+  
+ if(fixedHour > presentHour){
+   $(this).addClass("past");
+ } else if (presentHour === fixedHour){
+   $(this).addClass("present");
+ } else { (presentHour < fixedHour)
+   $(this).addClass("future");
+ }
+ });
+ }; 
+  
+  var saveBtn = $(".saveBtn")
+  saveBtn.on("click", function() {
+
+  
+  let timeSchedule = $(this).siblings(".hour").text();
+  let text = $(this).siblings("#Text").val();
+  
+  localStorage.setItem(timeSchedule, text);
+  });
+  
+  function storedAgenda (){
+  $(".hour").each(function(){
+    let presentHour = $(this).text();
+    let storedAgenda = localStorage.getItem(presentHour);
+  
+    if (storedAgenda !== null) {
+      $(this).siblings("textarea").val(storedAgenda);
+     }
+  })
+  }
+
+  //Call functions
+  time();
+  storedAgenda();
